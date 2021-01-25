640f14355452e4b430a4d1904b0ea1fe71ce5933
ryannggy <ryngworks@gmail.com>
2021-01-26 00:48:43 +0800

    fix predict_flow2 in FLowNetS.py

--- networks/FlowNetS.py
+++ networks/FlowNetS.py
@@ -12,22 +12,21 @@ import numpy as np
 from .submodules import *
 'Parameter count : 38,676,504 '
 
-
 class FlowNetS(nn.Module):
     def __init__(self, args, input_channels=12, batchNorm=True):
         super(FlowNetS, self).__init__()
 
         self.batchNorm = batchNorm
-        self.conv1 = conv(self.batchNorm,  input_channels,32, kernel_size=7, stride=2)
-        self.conv2 = conv(self.batchNorm,  32,  64, kernel_size=5, stride=2)
-        self.conv3 = conv(self.batchNorm, 64,  128, kernel_size=5, stride=2)
-        self.conv3_1 = conv(self.batchNorm, 128,  128)
-        self.conv4 = conv(self.batchNorm, 128,  256, stride=2)
-        self.conv4_1 = conv(self.batchNorm, 256,  256)
-        self.conv5 = conv(self.batchNorm, 256, 256, stride=2)
-        self.conv5_1 = conv(self.batchNorm, 256,  256)
-        self.conv6 = conv(self.batchNorm, 256, 512, stride=2)
-        self.conv6_1 = conv(self.batchNorm, 512, 512)
+        self.conv1    = conv(self.batchNorm,  input_channels,32, kernel_size=7, stride=2)
+        self.conv2    = conv(self.batchNorm,  32,  64, kernel_size=5, stride=2)
+        self.conv3    = conv(self.batchNorm, 64,  128, kernel_size=5, stride=2)
+        self.conv3_1  = conv(self.batchNorm, 128,  128)
+        self.conv4    = conv(self.batchNorm, 128,  256, stride=2)
+        self.conv4_1  = conv(self.batchNorm, 256,  256)
+        self.conv5    = conv(self.batchNorm, 256, 256, stride=2)
+        self.conv5_1  = conv(self.batchNorm, 256,  256)
+        self.conv6    = conv(self.batchNorm, 256, 512, stride=2)
+        self.conv6_1  = conv(self.batchNorm, 512, 512)
 
         self.deconv5 = deconv(512, 256)
         self.deconv4 = deconv(514, 128)
@@ -38,7 +37,7 @@ class FlowNetS(nn.Module):
         self.predict_flow5 = predict_flow(514)
         self.predict_flow4 = predict_flow(386)
         self.predict_flow3 = predict_flow(194)
-        self.predict_flow2 = predict_flow(89)
+        self.predict_flow2 = predict_flow(98)
 
         self.upsampled_flow6_to_5 = nn.ConvTranspose2d(
             2, 2, 4, 2, 1, bias=False)
