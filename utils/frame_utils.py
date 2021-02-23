c0a9320a0e450630e8407950a19409c082ca3801
Ryan <47684424+ryannggy@users.noreply.github.com>
2021-02-23 14:08:45 +0800

    Update frame_utils.py

--- utils/frame_utils.py
+++ utils/frame_utils.py
@@ -7,8 +7,8 @@ def read_gen(file_name):
     ext = splitext(file_name)[-1]
     if ext == '.png' or ext == '.jpeg' or ext == '.ppm' or ext == '.jpg':
         im = imread(file_name)
-        if im.shape[2] > 3:
-            return im[:,:,:3]
+        if im.shape[2] > 1:     #convert to using grayscale instead
+            return im[:,:,:1]
         else:
             return im
     elif ext == '.bin' or ext == '.raw':
