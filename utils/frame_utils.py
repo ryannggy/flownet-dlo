db446e0d91078b9ff88a2b85a8323e4920d5eab7
Ryan <47684424+ryannggy@users.noreply.github.com>
2021-02-24 22:12:39 +0800

    Update frame_utils.py

--- utils/frame_utils.py
+++ utils/frame_utils.py
@@ -2,6 +2,7 @@ import numpy as np
 from os.path import *
 from scipy.misc import imread
 from . import flow_utils 
+from numpy import newaxis
 
 def read_gen(file_name):
     ext = splitext(file_name)[-1]
@@ -10,7 +11,7 @@ def read_gen(file_name):
 #         if im.shape[2] > 1:     #convert to using grayscale instead
 #             return im[:,:,:1]
 #         else:
-        return im
+        return im[:,:,newaxis]
     elif ext == '.bin' or ext == '.raw':
         return np.load(file_name)
     elif ext == '.flo':
