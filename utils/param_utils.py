a6210e392619e80e5e076c8319d785fd26f74af6
Ryan <47684424+ryannggy@users.noreply.github.com>
2021-02-28 23:00:43 +0800

    Update param_utils.py

--- utils/param_utils.py
+++ utils/param_utils.py
@@ -2,6 +2,59 @@ import torch
 import torch.nn as nn
 import numpy as np
 
+def parse_flownetsonly(modules, weights, biases, param_prefix=''):
+    keys = [
+    'conv1',
+    'conv2',
+    'conv3',
+    'conv3_1',
+    'conv4',
+    'conv4_1',
+    'conv5',
+    'conv5_1',
+    'conv6',
+    'conv6_1',
+    
+    'deconv5',
+    'deconv4',
+    'deconv3',
+    'deconv2',
+    
+    'Convolution1',
+    'Convolution2',
+    'Convolution3',
+    'Convolution4',
+    'Convolution5',
+
+    'upsample_flow6to5',
+    'upsample_flow5to4',
+    'upsample_flow4to3',
+    'upsample_flow3to2',
+    ]
+    for i, k in enumerate(keys):
+        if 'upsample' in k:
+            keys[i] = param_prefix + param_prefix + k
+        else:
+            keys[i] = param_prefix + k
+    i = 0
+    for m in modules:
+        if isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d):
+            weight = weights[keys[i]].copy()
+            bias = biases[keys[i]].copy()
+            if keys[i] == param_prefix+'conv1':
+                # print ("%s :"%(keys[i]), m.weight.size(), m.bias.size(), tf_w[keys[i]].shape[::-1])
+                m.weight.data[:,:,:,:] = torch.from_numpy(np.flip(weight[:,:,:,:], axis=1).copy())
+                #m.weight.data[:,0:3,:,:] = torch.from_numpy(np.flip(weight[:,0:3,:,:], axis=1).copy())
+                #m.weight.data[:,3:6,:,:] = torch.from_numpy(np.flip(weight[:,3:6,:,:], axis=1).copy())
+                if m.bias is not None:
+                    m.bias.data[:] = torch.from_numpy(bias)
+            else:
+                m.weight.data[:,:,:,:] = torch.from_numpy(weight)
+                if m.bias is not None:
+                    m.bias.data[:] = torch.from_numpy(bias)
+            i = i + 1
+    return
+
 def parse_flownetc(modules, weights, biases):
     keys = [
     'conv1',
@@ -92,58 +145,6 @@ def parse_flownets(modules, weights, biases, param_prefix='net2_'):
                 m.weight.data[:,3:6,:,:] = torch.from_numpy(np.flip(weight[:,3:6,:,:], axis=1).copy())
                 m.weight.data[:,6:9,:,:] = torch.from_numpy(np.flip(weight[:,6:9,:,:], axis=1).copy())
                 m.weight.data[:,9::,:,:] = torch.from_numpy(weight[:,9:,:,:].copy())
-                if m.bias is not None:
-                    m.bias.data[:] = torch.from_numpy(bias)
-            else:
-                m.weight.data[:,:,:,:] = torch.from_numpy(weight)
-                if m.bias is not None:
-                    m.bias.data[:] = torch.from_numpy(bias)
-            i = i + 1
-    return
-
-def parse_flownetsonly(modules, weights, biases, param_prefix=''):
-    keys = [
-    'conv1',
-    'conv2',
-    'conv3',
-    'conv3_1',
-    'conv4',
-    'conv4_1',
-    'conv5',
-    'conv5_1',
-    'conv6',
-    'conv6_1',
-    
-    'deconv5',
-    'deconv4',
-    'deconv3',
-    'deconv2',
-    
-    'Convolution1',
-    'Convolution2',
-    'Convolution3',
-    'Convolution4',
-    'Convolution5',
-
-    'upsample_flow6to5',
-    'upsample_flow5to4',
-    'upsample_flow4to3',
-    'upsample_flow3to2',
-    ]
-    for i, k in enumerate(keys):
-        if 'upsample' in k:
-            keys[i] = param_prefix + param_prefix + k
-        else:
-            keys[i] = param_prefix + k
-    i = 0
-    for m in modules:
-        if isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d):
-            weight = weights[keys[i]].copy()
-            bias = biases[keys[i]].copy()
-            if keys[i] == param_prefix+'conv1':
-                # print ("%s :"%(keys[i]), m.weight.size(), m.bias.size(), tf_w[keys[i]].shape[::-1])
-                m.weight.data[:,0:3,:,:] = torch.from_numpy(np.flip(weight[:,0:3,:,:], axis=1).copy())
-                m.weight.data[:,3:6,:,:] = torch.from_numpy(np.flip(weight[:,3:6,:,:], axis=1).copy())
                 if m.bias is not None:
                     m.bias.data[:] = torch.from_numpy(bias)
             else:
