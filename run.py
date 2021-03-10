40286d3a2bb2895e0c1b9a4892271f9ae0cbc5cd
Ryan <47684424+ryannggy@users.noreply.github.com>
2021-03-11 01:09:10 +0800

    Update run.py

--- run.py
+++ run.py
@@ -11,7 +11,7 @@ for subdir, dirs, files in os.walk(finalPath):
     for d in dirs:
         dir_tmp = d.split('/')
         dirNo = dir_tmp[len(dir_tmp)-1]
-        os.system(f"CUDA_AVAILABLE_DEVICES=0,1 python main.py --inference --model FlowNet2S --save_flow --inference_dataset ImagesFromFolder \
+        os.system(f"python main.py --inference --model FlowNet2S --save_flow --inference_dataset ImagesFromFolder \
         --inference_dataset_root /home/sawsn/tachysense-gray-50-6464/train/final/{dirNo} \
         --inference_visualize \
         --resume /home/sawsn/FlowNet2S_model_best.pth.tar \
@@ -21,7 +21,7 @@ for subdir, dirs, files in os.walk(cleanPath):
     for d in dirs:
         dir_tmp = d.split('/')
         dirNo = dir_tmp[len(dir_tmp)-1]
-        os.system(f"CUDA_AVAILABLE_DEVICES=0,1 python main.py --inference --model FlowNet2S --save_flow --inference_dataset ImagesFromFolder \
+        os.system(f"python main.py --inference --model FlowNet2S --save_flow --inference_dataset ImagesFromFolder \
         --inference_dataset_root /home/sawsn/tachysense-gray-50-6464/train/clean/{dirNo} \
         --inference_visualize \
         --resume /home/sawsn/FlowNet2S_model_best.pth.tar \
