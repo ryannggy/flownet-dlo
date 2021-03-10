1d8e7ac2daeb51fda690d5feb32ac18dce4d3788
Ryan <47684424+ryannggy@users.noreply.github.com>
2021-03-11 03:49:23 +0800

    Update run.py

--- run.py
+++ run.py
@@ -7,20 +7,20 @@ path += '/../tachysense-gray-50-6464/train'
 cleanPath = path + '/clean'
 finalPath = path + '/final'
 
-for subdir, dirs, files in os.walk(finalPath):
-    for d in dirs:
-        dir_tmp = d.split('/')
-        dirNo = dir_tmp[len(dir_tmp)-1]
-        os.system(f"python main.py --inference --model FlowNet2S --save_flow --inference_dataset ImagesFromFolder \
-        --inference_dataset_root /home/sawsn/tachysense-gray-50-6464/train/final/{dirNo} --inference_visualize \
-        --resume /home/sawsn/FlowNet2S_model_best.pth.tar --save /home/sawsn/flownet-dlo/inference_results/final/{dirNo} \
-        --skip_training --skip_validation --inference_n_batches 1")
+# for subdir, dirs, files in os.walk(finalPath):
+#     for d in dirs:
+#         dir_tmp = d.split('/')
+#         dirNo = dir_tmp[len(dir_tmp)-1]
+os.system(f"python main.py --inference --model FlowNet2S --save_flow --inference_dataset MpiSintelFinal \
+--inference_dataset_root /home/sawsn/tachysense-gray-50-6464/train/ --inference_visualize \
+--resume /home/sawsn/FlowNet2S_model_best.pth.tar --save /home/sawsn/flownet-dlo/inference_results/final \
+--skip_training --skip_validation")
 
-for subdir, dirs, files in os.walk(cleanPath):
-    for d in dirs:
-        dir_tmp = d.split('/')
-        dirNo = dir_tmp[len(dir_tmp)-1]
-        os.system(f"python main.py --inference --model FlowNet2S --save_flow --inference_dataset ImagesFromFolder \
-        --inference_dataset_root /home/sawsn/tachysense-gray-50-6464/train/clean/{dirNo} --inference_visualize \
-        --resume /home/sawsn/FlowNet2S_model_best.pth.tar --save /home/sawsn/flownet-dlo/inference_results/clean/{dirNo} \
-        --skip_training --skip_validation --inference_n_batches 1")
+# for subdir, dirs, files in os.walk(cleanPath):
+#     for d in dirs:
+#         dir_tmp = d.split('/')
+#         dirNo = dir_tmp[len(dir_tmp)-1]
+os.system(f"python main.py --inference --model FlowNet2S --save_flow --inference_dataset MpiSintelClean \
+--inference_dataset_root /home/sawsn/tachysense-gray-50-6464/train/--inference_visualize \
+--resume /home/sawsn/FlowNet2S_model_best.pth.tar --save /home/sawsn/flownet-dlo/inference_results/clean \
+--skip_training --skip_validation")
