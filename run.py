96ec78254f7e72e3c31834946df868c5f65c2d2c
Ryan <47684424+ryannggy@users.noreply.github.com>
2021-03-11 14:02:11 +0800

    Update run.py

--- run.py
+++ run.py
@@ -10,18 +10,18 @@ finalPath = path + '/final'
 for subdir, dirs, files in os.walk(finalPath):
     for d in dirs:
         dir_tmp = d.split('/')
-        dirNo = dir_tmp[len(dir_tmp)-1]
+        dirNo = dir_tmp[len(dir_tmp)-1]; print(f"Working on: {dirNo}")
         os.system(f"python main.py --inference --model FlowNet2S --save_flow --inference_dataset ImagesFromFolder \
         --inference_dataset_root /home/sawsn/tachysense-gray-50-6464/train/final/{dirNo} --inference_visualize \
-        --resume /home/sawsn/FlowNet2S_model_best.pth.tar --save /home/sawsn/flownet-dlo/inference_results/final \
+        --resume /home/sawsn/FlowNet2S_model_best.pth.tar --save /home/sawsn/flownet-dlo/inference_results/final/{dirNo} \
         --skip_training --skip_validation")
         quit()
         
 for subdir, dirs, files in os.walk(cleanPath):
     for d in dirs:
         dir_tmp = d.split('/')
-        dirNo = dir_tmp[len(dir_tmp)-1]
+        dirNo = dir_tmp[len(dir_tmp)-1]; print(f"Working on: {dirNo}")
         os.system(f"python main.py --inference --model FlowNet2S --save_flow --inference_dataset ImagesFromFolder \
         --inference_dataset_root /home/sawsn/tachysense-gray-50-6464/train/clean/{dirNo} --inference_visualize \
-        --resume /home/sawsn/FlowNet2S_model_best.pth.tar --save /home/sawsn/flownet-dlo/inference_results/clean \
+        --resume /home/sawsn/FlowNet2S_model_best.pth.tar --save /home/sawsn/flownet-dlo/inference_results/clean/{dirNo} \
         --skip_training --skip_validation")
